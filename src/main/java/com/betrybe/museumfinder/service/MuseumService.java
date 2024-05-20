55fb8bf1ce87a384dd84aa649f17c68ba71f6ff2
brandaorichard <rih.brandao@gmail.com>
2024-05-20 20:04:58 -0300

    feat: create museum service

---
+++ src/main/java/com/betrybe/museumfinder/service/MuseumService.java
@@ -0,0 +1,47 @@
+package com.betrybe.museumfinder.service;
+
+import com.betrybe.museumfinder.database.MuseumFakeDatabase;
+import com.betrybe.museumfinder.exception.InvalidCoordinateException;
+import com.betrybe.museumfinder.model.Coordinate;
+import com.betrybe.museumfinder.model.Museum;
+import com.betrybe.museumfinder.util.CoordinateUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * The type Museum service.
+ */
+@Service
+public class MuseumService implements MuseumServiceInterface {
+
+  private final MuseumFakeDatabase database;
+
+  /**
+   * Instantiates a new Museum service.
+   *
+   * @param database the database
+   */
+  @Autowired
+  public MuseumService(MuseumFakeDatabase database) {
+    this.database = database;
+  }
+
+  @Override
+  public Museum getClosestMuseum(Coordinate coordinate, Double maxDistance) {
+    return null;
+  }
+
+  @Override
+  public Museum createMuseum(Museum museum) {
+    if (!CoordinateUtil.isCoordinateValid(museum.getCoordinate())) {
+      throw new InvalidCoordinateException("Invalid coordinates provided for the museum.");
+    }
+
+    return database.saveMuseum(museum);
+  }
+
+  @Override
+  public Museum getMuseum(Long id) {
+    return null;
+  }
+}
